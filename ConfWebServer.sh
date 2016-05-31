232ac60bc75e9a758b6d02aacb0daf98b7c1081b
Alvaro Marinho <alvinkalango@gmail.com>
2016-05-31 19:27:31 -0300

    update script LAMP

--- ConfWebServer.sh
+++ ConfWebServer.sh
@@ -1,77 +1,54 @@
 clear
 echo
 echo
-echo "                    ***** SCRIPT DE INSTALAÇÃO E CONFIGURAÇÃO DO SERVIDOR WEB E CAKEPHP 2.5.4 *****"
+echo "***** LAMP INSTALATION AND CONFIGURATION *****"
 echo
-echo "          Deseja continuar? S*/N"
+echo "Continue (Y*/N)?"
+read opt
 
-read opcao
-
-if [ -z $opcao ]; then # caso o usuário precisone "s" ou "S"
-	opcao=s
+if [ -z $opt ]; then
+	opt=y
 fi
 
-if [ "$opcao" == n -o "$opcao" == N ]; then
-	read -p "Pressione qualquer tecla para continuar. . . escolha: [$opcao]"
+if [ "$opt" == n -o "$opt" == N ]; then
+	echo
+	read -p "Installation canceled. Press any key to continue . . ."
+	clear
 	exit 0
 fi
 
 echo
-echo "Verificando pacotes necessarios. Comandos 'sudo' serao executados.
-Digite a senha do usuario logado, caso seja necessario... "
+echo
+echo "Checking necessary packages! Command sudo will be executed. Enter the password if necessary . . ."
 echo
 echo
 
-#instalação dos pacotes necessários
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade -y
 clear
-sudo apt-get install aptitude
-sudo aptitude install git apache2 php5 libapache2-mod-php5 mysql-server-5.5 phpmyadmin
+sudo apt-get install lamp-server^
+clear
+
+usuario=`whoami`
+sudo adduser $usuario www-data
+sudo chown $usuario:www-data -R /var/www/
+sudo chmod 755 -R /var/www/
+
+sudo service apache2 restart 
 
 sudo cp -r /usr/share/phpmyadmin/ /var/www/phpmyadmin
 
-echo
-echo
-echo "		Instalaçao dos pacotes necessarios concluida..."
-echo
+sudo a2enmod rewrite
 
-usuario=`whoami`
+cd /etc/apache2/sites-available/
 
-cd ~
-cd ../../etc/apache2/sites-available/
+sudo sed -i 's/html/ /g' 000-default.conf
 
-clear
-echo
-echo
-echo "		Apague /HTML do trecho 'DocumentRoot /var/www/html', SALVE e FECHE o documento"
-echo
-read -p "Pressione qualquer tecla para continuar..."
-
-sudo gedit 000-default.conf
-
-cd ../sites-enabled/
-
-clear
-echo
-echo
-echo "		NOVAMENTE Apague /HTML do trecho 'DocumentRoot /var/www/html', SALVE e FECHE o documento"
-echo
-read -p "Pressione qualquer tecla para continuar..."
-
-sudo gedit 000-default.conf
-
-#dando permissão de escrita na pasta www
-sudo chown $usuario:$usuario /var/www/ -R
-
-#criando um arquivo de teste
-echo -e '<?php phpinfo(); ?>' > /var/www/teste.php 
-
-#reiniciar o servidor Apache
+echo "<?php echo 'Its work!'; ?>" | sudo tee /var/www/test.php
 sudo service apache2 restart 
 
 clear
 echo
 echo
-echo "		Instalação do Servidor Web Concluida!"
+echo "Installation completed!"
 echo
 echo
