4f9072899c90af513392efb74e6cb16a3f782505
Alvaro Marinho <alvaro.marinho@live.com>
2017-01-04 11:30:57 -0200

    Select text from SPAN tag

--- JqueryCodes.js
+++ JqueryCodes.js
@@ -40,4 +40,20 @@ $(document).ready(function(){
         cloneCount--;
     });
     
+    // SELECT TEXT FROM SPAN TAG
+    $(".copyText").click(function(){ 
+        var range, selection;
+        if (window.getSelection && document.createRange) {
+            selection = window.getSelection();
+            range = document.createRange();
+            range.selectNodeContents($(this)[0]);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        } else if (document.selection && document.body.createTextRange) {
+            range = document.body.createTextRange();
+            range.moveToElementText($(this)[0]);
+            range.select();
+        }
+    });
+    
 });
